21246dd038bf3895149cf999838ed80d0bdcdc80
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2021-03-22 09:02:49 -0600

    Update web.php

--- centros/routes/web.php
+++ centros/routes/web.php
@@ -19,7 +19,6 @@ Route::get('/', function () {
  });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-
      return view('centros.index');
 })->name('dashboard');
 
@@ -29,5 +28,6 @@ Route::resource("/centro", "App\Http\Controllers\CentroControler");
 //Route::get('/centro', 'SweetController@notification');
 
 Route::get('/colonias/escuelas/{idColonia}', [CentroControler::class, 'getEscuelas']);
+Route::get('/empleado', [EmpleadoController::class, 'getDeptos']);
 
 
